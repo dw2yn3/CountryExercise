6b8aa3413ef922cb26000d2d54763373f46a0355
Dwayne Wedderburn <dw2yn3@gmail.com>
2018-07-09 00:40:05 -0400

    Country Exercise using difference features of C#

--- CountryExercise/Program.cs
+++ CountryExercise/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorldNamespace;
 
 namespace CountryExercise
 {
@@ -10,6 +11,23 @@ namespace CountryExercise
     {
         static void Main(string[] args)
         {
-        }
+            Country countryOneInstance = new Country();
+            Country countryTwoInstance = new Country();
+
+            CountryLanguages countryLanguages = new CountryLanguages();
+            countryLanguages.FirstLanguage = Languages.Dutch.ToString();
+            countryLanguages.SecondLanguage = Languages.Vietnamese.ToString();
+
+            countryOneInstance.CountryLanguages = countryLanguages;
+            countryOneInstance.PlanetName = "Earth";
+            countryOneInstance.CountryName = "Canada";
+            countryOneInstance.Currency = Currencies.CAD;
+            countryOneInstance.Continents = Continents.NorthAmerica;
+
+            countryOneInstance.SayHi();
+            countryOneInstance.SayHi(countryOneInstance.CountryName);
+
+            Console.WriteLine($"Planet {countryOneInstance.PlanetName} and country is {countryOneInstance.CountryName} and currency is {countryOneInstance.Currency}");
+        }   
     }
 }
